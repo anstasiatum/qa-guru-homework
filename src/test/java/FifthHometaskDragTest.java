83432ae9df63050dd27be175aec645b0b158e7dd
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-05-12 20:18:51 +0300

    5th Hometask Hover & DragnDrop

--- src/test/java/FifthHometaskDragTest.java
+++ src/test/java/FifthHometaskDragTest.java
@@ -1,25 +1,25 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class FifthHometaskHoverTest {
+public class FifthHometaskDragTest {
     @BeforeAll
     static void configuration() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void enterprisePageShouldHaveATitleTest() {
-        open("/home");
-        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
-        $(".HeaderMenu-nav").$(byText("Enterprises")).click();
-        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+    void rectanglesShouldSwitchPlacesTest() {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 }

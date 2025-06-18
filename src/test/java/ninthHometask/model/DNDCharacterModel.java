b273ab87ddaf4c84dbda37474da6db980634480b
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-06-18 22:55:43 +0300

    9th Hometask: Working with files

---
+++ src/test/java/ninthHometask/model/DNDCharacterModel.java
@@ -0,0 +1,21 @@
+package ninthHometask.model;
+
+import java.util.List;
+
+public class DNDCharacterModel {
+    private String race;
+    private String level;
+    private List<String> languages;
+
+    public String getRace() {
+        return race;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+}

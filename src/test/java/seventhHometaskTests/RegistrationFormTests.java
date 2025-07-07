bec2f5a1d6ceba6d154d3a1463ae783b25055b1b
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-07-07 16:25:24 +0300

    Configuring Jenkins

--- src/test/java/seventhHometaskTests/RegistrationFormTests.java
+++ src/test/java/seventhHometaskTests/RegistrationFormTests.java
@@ -2,7 +2,9 @@ package seventhHometaskTests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -47,6 +49,11 @@ public class RegistrationFormTests extends TestBase {
     static void browserConfiguration() {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
     }
 
     @Test

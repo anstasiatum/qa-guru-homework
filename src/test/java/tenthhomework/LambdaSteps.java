a7128a1a11fb4b43bd503c3e2bb3815d0d8f2e4a
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-07-07 16:04:35 +0300

    Configuring Jenkins

--- src/test/java/tenthhomework/LambdaSteps.java
+++ src/test/java/tenthhomework/LambdaSteps.java
@@ -1,7 +1,10 @@
 package tenthhomework;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tenthhomework.annotationsteps.Steps;
 
@@ -11,8 +14,14 @@ import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
+@Tag("Jenkins")
 public class LambdaSteps {
     public static final String REPOSITORY = "anstasiatum/qa-guru-homework";
+
+    @BeforeAll
+    static void browserConfiguration() {
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
 
     @Test
     public void testLambdaStep() {
